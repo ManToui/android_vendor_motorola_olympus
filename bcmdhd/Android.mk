7b22233059b4a32c9f9e20cdf38e7772faec33ce
Emerson Pinter <dev@pinter.com.br>
2013-12-04 01:38:01 -0200

    Rename bcmdhd firmware
    Change-Id: Ib3935647d90f149201483b11d3ab55ca86e60c78

--- bcmdhd/Android.mk
+++ bcmdhd/Android.mk
@@ -1,24 +1,27 @@
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),olympus)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fw_bcmdhd.bin
+LOCAL_MODULE       := olympus_fw_bcmdhd.bin
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := fw_bcm4329.bin
+LOCAL_SRC_FILES    := fw_bcmdhd.bin
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_STEM  := $(LOCAL_SRC_FILES)
 include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fw_bcmdhd_apsta.bin
+LOCAL_MODULE       := olympus_fw_bcmdhd_apsta.bin
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := fw_bcm4329_apsta.bin
+LOCAL_SRC_FILES    := fw_bcmdhd_apsta.bin
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_STEM  := $(LOCAL_SRC_FILES)
 include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fw_bcmdhd_p2p.bin
+LOCAL_MODULE       := olympus_fw_bcmdhd_p2p.bin
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := fw_bcm4329_p2p.bin
+LOCAL_SRC_FILES    := fw_bcmdhd_p2p.bin
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_STEM  := $(LOCAL_SRC_FILES)
 include $(BUILD_PREBUILT)
 endif
