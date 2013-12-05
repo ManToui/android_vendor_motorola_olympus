a7f730b65ab50fbd428fb7bbb43a478ae1788398
Emerson Pinter <dev@pinter.com.br>
2013-12-04 23:01:58 -0200

    Add bcm4329
    Change-Id: I5924e378e56502e049f6a69b4e8f65d6b287847f

---
+++ bcm4329/Android.mk
@@ -0,0 +1,19 @@
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),olympus)
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := olympus_fw_bcm4329.bin
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := fw_bcm4329.bin
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_STEM  := $(LOCAL_SRC_FILES)
+include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := olympus_fw_bcm4329_apsta.bin
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := fw_bcm4329_apsta.bin
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_STEM  := $(LOCAL_SRC_FILES)
+include $(BUILD_PREBUILT)
+endif
